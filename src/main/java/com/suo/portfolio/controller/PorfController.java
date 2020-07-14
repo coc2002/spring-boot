af0b31d3059da49399ff31c8da976eea4faa80cd
Kim <shxm2016@naver.com>
2020-07-14 19:21:52 +0900

    layout 구성

--- src/main/java/com/suo/portfolio/controller/PorfController.java
+++ src/main/java/com/suo/portfolio/controller/PorfController.java
@@ -1,5 +1,13 @@
 package com.suo.portfolio.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class PorfController {
+
+    @GetMapping
+    public String index(){
+        return "index";
+        }
 }
